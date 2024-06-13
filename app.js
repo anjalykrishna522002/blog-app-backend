384ac16760034eb2f74c69a26cc582cdaa524d16
anjalykrishna522002 <anjalykrishnaas@gmail.com>
2024-06-13 15:22:07 +0530

    token created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require ("express")
 const mongoose = require ("mongoose")
 const cors = require ("cors")
 const bcrypt = require ("bcryptjs")
+const jwt = require ("jsonwebtoken")
 
 const {blogmodel} = require ("./models/blog")
 
@@ -28,6 +29,34 @@ app.post("/signup",async (req,res)=>{
 
     res.json({"status":"success"})
 })
+app.post("/signIn",(req,res)=>{
+    let input = req.body
+    blogmodel.find({"email":req.body.email}).then(
+        (response)=>{
+            if (response.length>0) {
+                let dbPassword = response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                    if (isMatch) {
+                        jwt.sign({email:input.email},"blog-app",{expiresIn:"1d"},(error,token)=>{
+                            if (error) {
+                                res.json({"status":"unable to create token"})
+                            } else {
+                                res.json({"status":"success","userid":response[0]._id,"token":token})
+                            }
+                        })
+                        
+                    } else {
+                        res.json({"status":"incorrect"})
+                    }
+                })
+            } else {
+                res.json({"status":"user not found"})
+            }
+        }
+    ).catch()
+})
+
 app.listen(8080,()=>{
     console.log("server running")
 })
